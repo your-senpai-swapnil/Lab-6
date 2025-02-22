a5e272a83b394c94a9f6ad1cce507da966129c43
swapnil15-3477 <swapnil15-3477@diu.edu.bd>
2025-02-22 05:00:08 +0000

    cached image updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart'; // Add this import
 
 void main() {
   runApp(const MyApp());
@@ -16,20 +16,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Lab 6 work'), 
-          backgroundColor: Colors.blue, 
+          backgroundColor: const Color.fromARGB(255, 228, 16, 9), 
         ),
         backgroundColor: const Color(0xFFE3E8FF),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CachedNetworkImage(imageUrl: "https://c4.wallpaperflare.com/wallpaper/114/1008/41/one-piece-monkey-d-luffy-hd-wallpaper-preview.jpg")
-              Image.network(
-                "https://c4.wallpaperflare.com/wallpaper/830/266/321/anime-one-piece-monkey-d-luffy-wallpaper-preview.jpg"
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded( 
+              child: Align(
+                alignment: Alignment.center, 
+                child: CachedNetworkImage(
+                  imageUrl: "https://c4.wallpaperflare.com/wallpaper/830/266/321/anime-one-piece-monkey-d-luffy-wallpaper-preview.jpg",
+                  placeholder: (context, url) => const CircularProgressIndicator(), 
+                  errorWidget: (context, url, error) => const Icon(Icons.error), 
+                  fit: BoxFit.contain, 
+                ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20), 
+            Expanded( 
+              child: Align(
+                alignment: Alignment.center, 
+                child: CachedNetworkImage(
+                  imageUrl: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/03/boa-hancock-talks-excitedly-while-blushing.jpg",
+                  placeholder: (context, url) => const CircularProgressIndicator(), 
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                  
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
