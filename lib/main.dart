f26f4a6d48bf4ab24a7b24d6817bb335f7a64ace
swapnil15-3477 <swapnil15-3477@diu.edu.bd>
2025-02-22 04:24:39 +0000

    network image ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,8 +15,8 @@ class MyApp extends StatelessWidget {
       title: 'Lab 6 work',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Lab 6 work'), // This sets the title in the header
-          backgroundColor: Colors.blue, // You can customize the color here
+          title: const Text('Lab 6 work'), 
+          backgroundColor: Colors.blue, 
         ),
         backgroundColor: const Color(0xFFE3E8FF),
         body: Center(
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              CachedNetworkImage(imageUrl: "https://c4.wallpaperflare.com/wallpaper/114/1008/41/one-piece-monkey-d-luffy-hd-wallpaper-preview.jpg")
               Image.network(
                 "https://c4.wallpaperflare.com/wallpaper/830/266/321/anime-one-piece-monkey-d-luffy-wallpaper-preview.jpg"
               ),
